12b68ff6f85b208d55c0688849ec538fdc727918
pum-k <ddkhoa1206@gmail.com>
2021-12-05 21:55:16 +0700

    Co-authored-by: ezzio <ezzio@users.noreply.github.com>

--- client/src/features/videoCall/videoCall.tsx
+++ client/src/features/videoCall/videoCall.tsx
@@ -1,29 +1,23 @@
-import { useAppDispatch } from "app/hooks";
-import React, { useEffect, useRef, useState } from "react";
-import { selectuserInRoom } from "pages/Room/RoomSlice";
+import { useAppDispatch } from 'app/hooks';
+import React, { useEffect, useRef, useState } from 'react';
+import { selectuserInRoom } from 'pages/Room/RoomSlice';
 
-const VideoCall = ({
-  connectionPeerjs,
-  CallTo,
-  nameId,
-  MyVideoCall,
-  styles,
-}: any) => {
+const VideoCall = ({ connectionPeerjs, CallTo, nameId, MyVideoCall }: any) => {
   const MyVideo = React.useRef<any>();
 
   useEffect(() => {
     try {
-      let videoGird = document.getElementById("video-grid");
+      let videoGird = document.getElementById('video-grid');
       if (videoGird) {
         videoGird.classList.remove(nameId);
       }
       let call = connectionPeerjs.call(CallTo, MyVideoCall, {
-        metadata: localStorage.getItem("owner"),
+        metadata: localStorage.getItem('owner'),
       });
       // if()
       // console.log(document.getElementsByClassName(nameId));
 
-      call.on("stream", (remoteStream: any) => {
+      call.on('stream', (remoteStream: any) => {
         if (MyVideo.current != null) {
           MyVideo.current.srcObject = remoteStream;
         }
@@ -32,8 +26,8 @@ const VideoCall = ({
   }, []);
   return (
     <video
-      style={styles}
-      className={nameId}
+      id={nameId}
+      className="grid-container__video-tag"
       ref={MyVideo}
       autoPlay
       muted
